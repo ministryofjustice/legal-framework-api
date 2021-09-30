bf2826eafd73e69ceb6f37555af0d5c5ed2a5ec1
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2021-09-30 09:34:28 +0100

    Populate default_cost_limitations

--- db/populators/proceeding_type_populator.rb
+++ db/populators/proceeding_type_populator.rb
@@ -7,6 +7,7 @@ class ProceedingTypePopulator
 
   def call
     seed_data.each { |seed_row| populate(seed_row) }
+    populate_default_cost_limitations
   end
 
   private
@@ -33,5 +34,23 @@ class ProceedingTypePopulator
 
   def seed_data
     @seed_data ||= YAML.load_file(DATA_FILE)
+  end
+
+  def populate_default_cost_limitations
+    pts = ProceedingType.all
+    pts.each { |pt| add_cost_limitations(pt) }
+  end
+
+  def add_cost_limitations(proceeding_type)
+    find_or_create_dcl(proceeding_type: proceeding_type, cost_type: 'substantive', start_date: Date.parse('1970-01-01'), value: 25_000.0)
+    find_or_create_dcl(proceeding_type: proceeding_type, cost_type: 'delegated_functions', start_date: Date.parse('1970-01-01'), value: 1_350.0)
+    find_or_create_dcl(proceeding_type: proceeding_type, cost_type: 'delegated_functions', start_date: Date.parse('2021-09-13'), value: 2_250.0)
+  end
+
+  def find_or_create_dcl(proceeding_type:, cost_type:, start_date:, value:)
+    dcl = DefaultCostLimitation.find_by(proceeding_type: proceeding_type, cost_type: cost_type, start_date: start_date, value: value)
+    return if dcl.present?
+
+    DefaultCostLimitation.create!(proceeding_type: proceeding_type, cost_type: cost_type, start_date: start_date, value: value)
   end
 end
