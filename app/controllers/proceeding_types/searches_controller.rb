8055ef04a9cb46129cbb804e11a6b0afb15867f2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2021-10-05 12:01:50 +0100

    AP-2449: Implement Search controller and route
    This allows a submission
    ```ruby
      { "search_term": "Family" }
    ```
    and returns an array of matching proceeding_types
    
    optionally, you can add an excluded_terms value to prevent return of duplicate codes, e.g.
    ```ruby
      { "search_term": "Family", "excluded_terms": "DA006" }
    ```

---
+++ app/controllers/proceeding_types/searches_controller.rb
@@ -0,0 +1,79 @@
+module ProceedingTypes
+  class SearchesController < ApplicationController
+    resource_description do
+      short 'Search proceeding types'
+      formats(%w[json])
+      description <<-END_OF_TEXT
+      == Description
+      This endpoint accepts a json field containing a single value, `search_term`
+        {
+          search_term: 'occup'
+        }
+      and it will return a success marker and a data array of proceeding_types in json format, e.g.
+        {
+          success: true,
+          data: [
+            {
+              meaning: 'Occupation order',
+              ccms_code: 'DA005',
+              description: 'to be represented on an application for an occupation order.',
+              ccms_category_law: 'Family',
+              ccms_matter: 'Domestic abuse'
+            }
+          ]
+        }
+
+      Optionally you can send an excluded_terms value, this should be a comma separated string or codes to exclude, e.g.
+        {
+          "search_term": "Family",
+          "excluded_terms": "DA001,DA0002"
+        }
+      This will return proceeding terms that match Family but exclude any proceeding types with matching codes
+      END_OF_TEXT
+    end
+
+    api :POST, 'proceeding_types/search', 'Create a request to retrieve a list of proceeding types that match the search type'
+    param :search_term, String, required: true, desc: 'Search for proceeding types matching the `search_text`'
+
+    returns code: :ok, desc: 'Successful response' do
+      property :success, ['true'], desc: 'Success flag shows true'
+      property :data, array_of: String, desc: 'Returns an array of matching proceeding types'
+    end
+
+    returns code: :bad_request do
+      property :success, ['false'], desc: 'Success flag shows false'
+      property :error_class, String, desc: 'Name of the error class that caused the exception'
+      property :message, String, desc: 'Error message'
+    end
+
+    def create
+      render status: status, json: { success: status.eql?(200) }.merge(results).to_json
+    end
+
+    private
+
+    def status
+      @status ||= results.symbolize_keys[:data].blank? ? 400 : 200
+    end
+
+    def results
+      if build_results.empty?
+        { error: 'no matches found' }
+      else
+        { data: build_results }
+      end
+    rescue StandardError => e
+      { error: e.class.to_s, message: e.message }
+    end
+
+    def build_results
+      find = search_param
+      exclude = params['excluded_terms'] ||= []
+      @build_results ||= ProceedingTypeFullTextSearch.call(find, exclude)
+    end
+
+    def search_param
+      params.require(:search_term)
+    end
+  end
+end
---
+++ spec/requests/proceeding_types/searches_spec.rb
@@ -0,0 +1,126 @@
+require 'rails_helper'
+
+RSpec.describe 'ProceedingTypes/SearchController', type: :request do
+  before { seed_live_data }
+  describe 'POST proceeding_types/search' do
+    let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
+    let(:params) do
+      {
+        search_term: search_term
+      }
+    end
+    let(:search_term) { 'Occupation' }
+    subject { post proceeding_types_searches_path, params: params.to_json, headers: headers }
+
+    context 'when the params are valid' do
+      before { subject }
+      let(:expected_result) do
+        {
+          success: true,
+          data: [
+            {
+              meaning: 'Occupation order',
+              ccms_code: 'DA005',
+              description: 'to be represented on an application for an occupation order.',
+              ccms_category_law: 'Family',
+              ccms_matter: 'Domestic abuse'
+            }
+          ]
+        }
+      end
+
+      it 'returns a successful response' do
+        expect(response).to have_http_status(200)
+        expect(response.media_type).to eql('application/json')
+        expect(JSON.parse(response.body)).to match_json_expression(expected_result)
+      end
+    end
+
+    context 'when no matches found' do
+      before { subject }
+      let(:search_term) { 'nonexistant' }
+      let(:expected_result) do
+        {
+          success: false,
+          error: 'no matches found'
+        }
+      end
+
+      it 'returns an unsuccessful response' do
+        expect(response).to have_http_status(400)
+        expect(response.media_type).to eql('application/json')
+        expect(JSON.parse(response.body)).to match_json_expression(expected_result)
+      end
+    end
+
+    context 'when an error occurs' do
+      before do
+        allow(ProceedingTypeFullTextSearch).to receive(:call).and_raise(StandardError.new('Unexpected error in full text search'))
+        subject
+      end
+      let(:expected_result) do
+        {
+          success: false,
+          error: 'StandardError',
+          message: 'Unexpected error in full text search'
+        }
+      end
+
+      it 'returns an successful response' do
+        expect(response).to have_http_status(400)
+        expect(response.media_type).to eql('application/json')
+        expect(JSON.parse(response.body)).to match_json_expression(expected_result)
+      end
+    end
+
+    context 'when returning multiple results' do
+      before { subject }
+      let(:search_term) { 'injunction' }
+
+      it 'returns a successful response with three results' do
+        expect(response).to have_http_status(200)
+        expect(response.media_type).to eql('application/json')
+        expect(JSON.parse(response.body).symbolize_keys[:data].count).to eq 3
+      end
+
+      context 'and one of the codes is excluded' do
+        let(:params) do
+          {
+            search_term: 'injunction',
+            excluded_terms: 'DA001'
+          }
+        end
+
+        it 'returns a successful response with two results' do
+          expect(response).to have_http_status(200)
+          expect(response.media_type).to eql('application/json')
+          expect(JSON.parse(response.body).symbolize_keys[:data].count).to eq 2
+        end
+      end
+    end
+
+    context 'when sending excluded_terms' do
+      describe 'that should return a single match' do
+        before { subject }
+        let(:params) do
+          {
+            search_term: search_term,
+            excluded_terms: 'DA005'
+          }
+        end
+        let(:expected_result) do
+          {
+            success: false,
+            error: 'no matches found'
+          }
+        end
+
+        it 'returns an unsuccessful response' do
+          expect(response).to have_http_status(400)
+          expect(response.media_type).to eql('application/json')
+          expect(JSON.parse(response.body)).to match_json_expression(expected_result)
+        end
+      end
+    end
+  end
+end
