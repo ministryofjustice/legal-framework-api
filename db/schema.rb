21ac59bf03d2c5dbdf0d70dfd38b8b5b38d4fc8c
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-05-04 13:02:53 +0100

    Run rails db:migrate against rails 7
    Removes precision on timestamps as this is now internally
    handled by activerecord.
    
    see https://github.com/rails/rails/issues/44571#issuecomment-1059295012
    for more.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_102438) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_11_30_102438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -21,8 +20,8 @@ ActiveRecord::Schema.define(version: 2021_11_30_102438) do
     t.date "start_date", null: false
     t.string "cost_type", null: false
     t.decimal "value", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["proceeding_type_id"], name: "index_default_cost_limitations_on_proceeding_type_id"
   end
 
@@ -31,8 +30,8 @@ ActiveRecord::Schema.define(version: 2021_11_30_102438) do
     t.boolean "upper_gross_income_waiver", default: false, null: false
     t.boolean "upper_disposable_income_waiver", default: false, null: false
     t.boolean "upper_capital_waiver", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "category_of_law"
     t.string "code"
     t.string "category_of_law_code"
@@ -42,16 +41,16 @@ ActiveRecord::Schema.define(version: 2021_11_30_102438) do
   create_table "merits_tasks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
     t.string "type", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "proceeding_type_merits_tasks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "proceeding_type_id", null: false
     t.uuid "merits_task_id", null: false
     t.integer "sequence", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["proceeding_type_id"], name: "index_proceeding_type_merits_tasks_on_proceeding_type_id"
   end
 
@@ -60,8 +59,8 @@ ActiveRecord::Schema.define(version: 2021_11_30_102438) do
     t.uuid "scope_limitation_id"
     t.boolean "substantive_default"
     t.boolean "delegated_functions_default"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["proceeding_type_id", "delegated_functions_default"], name: "index_proceedings_scopes_unique_delegated_default", unique: true, where: "(delegated_functions_default = true)"
     t.index ["proceeding_type_id", "scope_limitation_id"], name: "index_proceedings_scopes_unique_on_ids", unique: true
     t.index ["proceeding_type_id", "substantive_default"], name: "index_proceedings_scopes_unique_substantive_default", unique: true, where: "(substantive_default = true)"
@@ -74,8 +73,8 @@ ActiveRecord::Schema.define(version: 2021_11_30_102438) do
     t.string "meaning", null: false
     t.string "description", null: false
     t.uuid "matter_type_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "additional_search_terms"
     t.tsvector "textsearchable"
     t.string "name", default: "default_name", null: false
@@ -90,8 +89,8 @@ ActiveRecord::Schema.define(version: 2021_11_30_102438) do
     t.string "request_payload"
     t.integer "response_status"
     t.string "response_payload"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "scope_limitations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -100,16 +99,16 @@ ActiveRecord::Schema.define(version: 2021_11_30_102438) do
     t.string "description", null: false
     t.boolean "substantive", default: false, null: false
     t.boolean "delegated_functions", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["code"], name: "index_scope_limitations_on_code"
   end
 
   create_table "task_dependencies", force: :cascade do |t|
     t.uuid "merits_task_id"
     t.uuid "dependency_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "default_cost_limitations", "proceeding_types"
