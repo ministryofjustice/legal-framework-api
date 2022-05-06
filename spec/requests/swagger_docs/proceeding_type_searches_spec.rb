569826711c205449169884f3b1efb96785f2e95e
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2022-05-06 17:25:33 +0100

    Update swagger docs to include description of each call

--- spec/requests/swagger_docs/proceeding_type_searches_spec.rb
+++ spec/requests/swagger_docs/proceeding_type_searches_spec.rb
@@ -3,6 +3,10 @@ require "swagger_helper"
 RSpec.describe "ProceedingTypes::SearchesController", type: :request, swagger: true do
   path "/proceeding_types/all" do
     get("Get summary of all proceeding types") do
+      description "Returns an array of all proceeding types with summary data.
+                   Call <code>/proceeding_types/XX999</code> where <code>XX999</code> is the ccms_code for more
+                   detailed data on a specific proceeding type."
+
       tags "Proceeding types"
 
       produces "application/json"
@@ -17,6 +21,8 @@ RSpec.describe "ProceedingTypes::SearchesController", type: :request, swagger: t
 
   path "/proceeding_types/searches" do
     post("Search proceeding types matching a search term, optionally excluding some ccms_codes") do
+      description "<code>POST</code> a JSON payload containing search terms and a list of codes to exclude to
+                  receive a response containing an array of proceeding types with summary data"
       search_term = "Injunction"
       excluded_codes = "DA001,DA007,SE013"
 
