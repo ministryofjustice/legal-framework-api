687972c22c03bae9dfd2ca1872198566ae676cfe
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-05-09 17:03:32 +0100

    Refactor out passing of var around
    To reduce complexity and verboseness of code.
    
    As a simple series of private methods
    it makes more sense to instantiate and use
    a memoize attribute reader type of setup.

--- app/services/proceeding_type_service.rb
+++ app/services/proceeding_type_service.rb
@@ -13,7 +13,7 @@ class ProceedingTypeService
   def call
     raise ProceedingTypeServiceError, "Must specify a proceeding type" if @ccms_code.nil?
 
-    populate_response(@ccms_code)
+    populate_response
     @response
   rescue StandardError => e
     @response = error_response_for(e)
@@ -21,16 +21,15 @@ class ProceedingTypeService
 
 private
 
-  def populate_response(ccms_code)
-    pt = ProceedingType.find_by!(ccms_code:)
-    add_matter_type_to_response(pt)
-    add_proceeding_type_to_response(pt)
-    add_cost_limitations_to_response(pt)
-    add_scope_limitations_to_response(pt)
-    add_service_levels_to_response(pt)
+  def populate_response
+    add_matter_type_to_response
+    add_proceeding_type_to_response
+    add_cost_limitations_to_response
+    add_scope_limitations_to_response
+    add_service_levels_to_response
   end
 
-  def add_matter_type_to_response(proceeding_type)
+  def add_matter_type_to_response
     matter_type = proceeding_type.matter_type
     @response[:ccms_category_law_code] = matter_type.category_of_law_code
     @response[:ccms_matter_code] = matter_type.code
@@ -38,48 +37,51 @@ private
     @response[:ccms_matter] = matter_type.name
   end
 
-  def add_proceeding_type_to_response(proceeding_type)
+  def add_proceeding_type_to_response
     @response[:ccms_code] = proceeding_type.ccms_code
     @response[:meaning] = proceeding_type.meaning
     @response[:name] = proceeding_type.name
     @response[:description] = proceeding_type.description
   end
 
-  def add_cost_limitations_to_response(proceeding_type)
-    scl = substantive_cost_limitations(proceeding_type)
-    dfcl = delegated_functions_cost_limitations(proceeding_type)
+  def add_cost_limitations_to_response
+    scl = substantive_cost_limitations
+    dfcl = delegated_functions_cost_limitations
     @response[:cost_limitations] = { substantive: { start_date: scl.start_date.to_s, value: scl.value.to_s },
                                      delegated_functions: { start_date: dfcl.start_date.to_s, value: dfcl.value.to_s } }
   end
 
-  def add_scope_limitations_to_response(proceeding_type)
-    scl = substantive_scope_limitations(proceeding_type)
-    dfcl = delegated_functions_scope_limitations(proceeding_type)
+  def add_scope_limitations_to_response
+    scl = substantive_scope_limitations
+    dfcl = delegated_functions_scope_limitations
     @response[:default_scope_limitations] = { substantive: { code: scl.code, meaning: scl.meaning, description: scl.description },
                                               delegated_functions: { code: dfcl.code, meaning: dfcl.meaning, description: dfcl.description } }
   end
 
-  def add_service_levels_to_response(proceeding_type)
-    @response[:service_levels] = []
+  def add_service_levels_to_response
     proceeding_type.service_levels.each do |lvl|
       @response[:service_levels] << { level: lvl.level, name: lvl.name, stage: lvl.stage, proceeding_default: lvl.proceeding_default }
     end
   end
 
-  def delegated_functions_cost_limitations(proceeding_type)
+  def delegated_functions_cost_limitations
     proceeding_type.default_cost_limitations.delegated_functions.for_date(Time.zone.today)
   end
 
-  def substantive_cost_limitations(proceeding_type)
+  def substantive_cost_limitations
     proceeding_type.default_cost_limitations.substantive.for_date(Time.zone.today)
   end
 
-  def delegated_functions_scope_limitations(proceeding_type)
+  def delegated_functions_scope_limitations
     proceeding_type.proceeding_type_scope_limitations.default_delegated_functions_scope_limitation
   end
 
-  def substantive_scope_limitations(proceeding_type)
+  def substantive_scope_limitations
     proceeding_type.proceeding_type_scope_limitations.default_substantive_scope_limitation
+  end
+
+  def proceeding_type
+    @proceeding_type ||= ProceedingType.find_by!(ccms_code: @ccms_code)
   end
 
   def skeleton_response
@@ -96,6 +98,7 @@ private
       ccms_matter: "",
       cost_limitations: {},
       default_scope_limitations: {},
+      service_levels: [],
     }
   end
 
