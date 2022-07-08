54ed9667f4dabc4f4077431acdaabd6afdc76d14
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-07-08 18:02:46 +0100

    AP-2392: refine the map call

--- app/controllers/client_involvement_types_controller.rb
+++ app/controllers/client_involvement_types_controller.rb
@@ -1,6 +1,6 @@
 class ClientInvolvementTypesController < ApplicationController
   def index
-    result = ClientInvolvementType.all.map { |cit| cit.api_json }
+    result = ClientInvolvementType.all.map(&:api_json)
     render json: result, status: :ok
   end
 end
