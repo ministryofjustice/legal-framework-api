5304c8b1abbd7beb6cc9d021f36305e430b50e8b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-07-13 13:42:46 +0100

    AP-3312: Update MultiThresholdWaiverService to use DB
    It checks for an exception to the waivers are false rule and
    generates the expected values

--- app/services/multi_threshold_waiver_service.rb
+++ app/services/multi_threshold_waiver_service.rb
@@ -33,22 +33,33 @@ private
   def add_proceeding_types_to_response(values)
     proceeding_type = ProceedingType.find_by!(ccms_code: values["ccms_code"])
     client_involvement_type = ClientInvolvementType.find_by!(ccms_code: values["client_involvement_type"])
+    waivers = get_threshold_waivers(proceeding_type, client_involvement_type)
 
-    add_threshold_waivers(proceeding_type, client_involvement_type)
+    add_threshold_waivers(proceeding_type, client_involvement_type, waivers)
   end
 
-  def threshold_for(matter_type, client_involvement_type)
-    matter_type.eql?("Domestic abuse") && client_involvement_type.ccms_code.eql?("A")
+  def get_threshold_waivers(proceeding_type, client_involvement_type)
+    thresholds = ThresholdWaiver.find_by!(matter_type: proceeding_type.matter_type, client_involvement_type:)
+    {
+      gross_income_upper: thresholds.gross_income_upper,
+      disposable_income_upper: thresholds.disposable_income_upper,
+      capital_upper: thresholds.capital_upper,
+    }
+  rescue ActiveRecord::RecordNotFound
+    {
+      gross_income_upper: false,
+      disposable_income_upper: false,
+      capital_upper: false,
+    }
   end
 
-  def add_threshold_waivers(proceeding_type, client_involvement_type)
-    threshold = threshold_for(proceeding_type.matter_type.name, client_involvement_type)
+  def add_threshold_waivers(proceeding_type, client_involvement_type, waivers)
     tw_hash = {
       ccms_code: proceeding_type.ccms_code,
       client_involvement_type: client_involvement_type.ccms_code,
-      gross_income_upper: threshold,
-      disposable_income_upper: threshold,
-      capital_upper: threshold,
+      gross_income_upper: waivers[:gross_income_upper],
+      disposable_income_upper: waivers[:disposable_income_upper],
+      capital_upper: waivers[:capital_upper],
       matter_type: proceeding_type.matter_type.name,
     }
 
