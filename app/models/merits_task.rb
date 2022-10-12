4809f41740d8e554cbdd520fe36ae7f6b4ffbf1e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-10-12 10:48:15 +0100

    AP-3476: Update MeritsTask model
    Add a has_many proceeding_type_merits_task, this allows us to
    query display_rules from an ApplicationTask

--- app/models/merits_task.rb
+++ app/models/merits_task.rb
@@ -1,6 +1,7 @@
 class MeritsTask < ApplicationRecord
   has_many :task_dependencies, dependent: :destroy
   has_many :dependencies, through: :task_dependencies
+  has_many :proceeding_type_merits_tasks, dependent: :destroy
 
   def dependency_names
     dependencies.map(&:name)
