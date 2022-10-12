2a925fe8d560bdc33057268f6ceead158b932de1
ColinBruce <colin.bruce@digital.justice.gov.uk>
2022-10-12 10:37:04 +0100

    AP-3476: Update the MeritsTaskService
    The current endpoint should only return tasks that
    have no display_rules set, this ensures it will only
    return the original questions

--- app/services/merits_task_service.rb
+++ app/services/merits_task_service.rb
@@ -40,7 +40,8 @@ private
   end
 
   def add_application_level_tasks_to_response(proceeding_type)
-    proceeding_type.application_tasks.each do |task|
+    applicable_tasks = proceeding_type.application_tasks.where("proceeding_type_merits_tasks.display_rules IS NULL")
+    applicable_tasks.each do |task|
       @response[:application][:tasks][task.name] = task.dependency_names
     end
   end
