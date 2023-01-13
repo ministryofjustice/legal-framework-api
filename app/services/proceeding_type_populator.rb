7b3420075f3f48cc706648d90b9bbc5906cf0fae
ColinBruce <colin.bruce@digital.justice.gov.uk>
2023-01-13 13:52:43 +0000

    AP-3749: Update ProceedingTypePopulator
    Remove the section that created default cost limitations
    as that is now handled by the default_cost_limitation_populator

--- app/services/proceeding_type_populator.rb
+++ app/services/proceeding_type_populator.rb
@@ -7,7 +7,6 @@ class ProceedingTypePopulator
 
   def call
     seed_data.each { |seed_hash| populate(seed_hash) }
-    populate_default_cost_limitations
   end
 
 private
@@ -29,23 +28,5 @@ private
 
   def seed_data
     @seed_data ||= YAML.load_file(DATA_FILE)
-  end
-
-  def populate_default_cost_limitations
-    pts = ProceedingType.all
-    pts.each { |pt| add_cost_limitations(pt) }
-  end
-
-  def add_cost_limitations(proceeding_type)
-    find_or_create_dcl(proceeding_type:, cost_type: "substantive", start_date: Date.parse("1970-01-01"), value: 25_000.0)
-    find_or_create_dcl(proceeding_type:, cost_type: "delegated_functions", start_date: Date.parse("1970-01-01"), value: 1_350.0)
-    find_or_create_dcl(proceeding_type:, cost_type: "delegated_functions", start_date: Date.parse("2021-09-13"), value: 2_250.0)
-  end
-
-  def find_or_create_dcl(proceeding_type:, cost_type:, start_date:, value:)
-    dcl = DefaultCostLimitation.find_by(proceeding_type:, cost_type:, start_date:, value:)
-    return if dcl.present?
-
-    DefaultCostLimitation.create!(proceeding_type:, cost_type:, start_date:, value:)
   end
 end
