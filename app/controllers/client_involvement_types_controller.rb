dff80f61566a30f3b333503028e9d8c4e29f6b81
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2023-07-13 16:17:00 +0100

    Refactor: amend client_involvement_type#api_json method
    JSON.parse(some_model_or_hash.to_json) is the same
    as some_model_or_hash.as_json. So amend api_json
    to use as_json and remove need to `JSON.parse` in the
    controller.

--- app/controllers/client_involvement_types_controller.rb
+++ app/controllers/client_involvement_types_controller.rb
@@ -1,6 +1,6 @@
 class ClientInvolvementTypesController < ApplicationController
   def index
-    result = ClientInvolvementType.order(:ordering).map { |cit| JSON.parse(cit.api_json) }
+    result = ClientInvolvementType.order(:ordering).map(&:api_json)
     render json: result, status: :ok
   end
 end
