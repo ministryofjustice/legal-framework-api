0670317b009dce13df722ca4071cab92cdb0d9a9
Katharine Ahern <katharine.ahern@justice.gov.uk>
2023-07-20 15:50:47 +0100

    ap-4334: add endpoint to return single organisation_type based on ccms_code

--- app/controllers/organisation_types_controller.rb
+++ app/controllers/organisation_types_controller.rb
@@ -3,4 +3,22 @@ class OrganisationTypesController < ApplicationController
     result = OrganisationType.order(:description).map(&:api_json)
     render json: result, status: :ok
   end
+
+  def show
+    response = organisation_type_response
+    status = response[:success] ? 200 : 400
+    render json: response, status:
+  end
+
+private
+
+  def organisation_type_response
+    { success: true, organisation_type: OrganisationType.find_by!(ccms_code: params[:ccms_code]).api_json }
+  rescue StandardError => e
+    if e.is_a?(ActiveRecord::RecordNotFound)
+      { success: false, message: "No such organisation type: '#{params[:ccms_code]}'" }
+    else
+      { success: false, message: e.message }
+    end
+  end
 end
