54054b89a654c34c4448c16669f1461941efb0f7
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-05-22 13:36:25 +0100

    AP-4662: Update ProceedingType seeding
    This updates the seed data to use the matter_type code
    rather than a string value that required finder methods
    
    We can look up the code and remove the finder methods
    and tests to simplify the seeds for future expansions
    without having to add extra helpers and tests

--- spec/models/proceeding_type_spec.rb
+++ spec/models/proceeding_type_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe ProceedingType do
     describe "#matter_type" do
       it "returns the correct matter type record" do
         rec = described_class.find_by(ccms_code: "DA006")
-        expect(rec.matter_type).to eq MatterType.domestic_abuse
+        expect(rec.matter_type).to eq MatterType.find_by(name: "Domestic abuse")
       end
     end
 
