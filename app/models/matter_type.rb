5bc89bcd0fe14040e8da3803980f61f9175c99b1
ColinBruce <colin.bruce@digital.justice.gov.uk>
2024-05-22 13:20:08 +0100

    AP-4662: Update MatterType model remove finder methods

--- app/models/matter_type.rb
+++ app/models/matter_type.rb
@@ -1,9 +1,2 @@
 class MatterType < ApplicationRecord
-  def self.domestic_abuse
-    find_by(name: "Domestic abuse")
-  end
-
-  def self.section8
-    find_by(name: "Children - section 8")
-  end
 end
