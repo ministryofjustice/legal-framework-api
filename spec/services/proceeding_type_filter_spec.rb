bafb85339a9f8387a227cdea8412427996276424
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-02-05 11:19:21 +0000

    AP-5727: Update proceeding type filter
    When a non PLF proceeding is present, no PLF proceedings should
    be returned.  This is in addition to the means/non-means PLF
    filetering already in place

--- spec/services/proceeding_type_filter_spec.rb
+++ spec/services/proceeding_type_filter_spec.rb
@@ -17,8 +17,10 @@ RSpec.describe ProceedingTypeFilter do
     context "and it has a non-SCA proceeding" do
       let(:current_proceedings) { %w[DA001] }
 
-      it "returns all proceedings minus the current one and sca_related proceedings" do
-        expect(proceeding_type_filter.count).to eq 134
+      it "returns all proceedings excluding the current one, all PLF and all sca_related proceedings" do
+        expect(proceeding_type_filter.count).to eq 39
+        expect(proceeding_type_filter.pluck("ccms_matter_code").uniq).not_to include "KPBLB"
+        expect(proceeding_type_filter.pluck("sca_related").uniq).not_to include true
       end
     end
 
@@ -33,8 +35,9 @@ RSpec.describe ProceedingTypeFilter do
     context "and it has a means tested PLF proceeding" do
       let(:current_proceedings) { %w[PBM04] }
 
-      it "returns only PLF proceedings minus the current one" do
+      it "returns only PLF proceedings minus the current one and the non_means_tested ones" do
         expect(proceeding_type_filter.pluck("ccms_matter_code").uniq).to eq %w[KPBLB]
+        expect(proceeding_type_filter.pluck("non_means_tested_plf").uniq).to eq [false]
         expect(proceeding_type_filter.count).to eq 90
       end
     end
@@ -45,6 +48,7 @@ RSpec.describe ProceedingTypeFilter do
       it "returns only the other non means tested PLF proceedings" do
         expect(proceeding_type_filter.pluck("ccms_matter_code").uniq).to eq %w[KPBLB]
         expect(proceeding_type_filter.pluck("ccms_code").uniq).to match_array %w[PBM45 PBM40E PBM45E]
+        expect(proceeding_type_filter.pluck("non_means_tested_plf").uniq).to eq [true]
         expect(proceeding_type_filter.count).to eq 3
       end
     end
